6c0e8c8bda38628fbef9ee08d943f86d9d63b2cc
Jesús Silva-Rodríguez <jsilva-ibis@us.es>
2023-08-29 19:34:06 +0200

    Created my first flake

---
+++ example/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Example C package";
+
+  outputs = { 
+    self, 
+    nixpkgs 
+  }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux = {
+      default = self.packages.x86_64-linux.hello;
+      thing1 = {
+        thing2 = nixpkgs.legacyPackages.x86_64-linux.btop;W
+      };
+
+    };
+  }
+} 
